7b1daeca00309890c08a44d4f2af674c177a85fd
alemat13 <alemat13@gmail.com>
2021-11-19 15:54:36 +0100

    add edit mode

--- src/app/invader-detail/invader-detail.component.ts
+++ src/app/invader-detail/invader-detail.component.ts
@@ -7,8 +7,19 @@ import { Invader } from '../invader';
   styleUrls: ['./invader-detail.component.css'],
 })
 export class InvaderDetailComponent implements OnInit {
+  public editMode = false;
   @Input() invader?: Invader;
   constructor() {}
 
   ngOnInit() {}
+  public getButtonLabel(): string {
+    return this.editMode ? "Save" : "Edit";
+  }
+  public save(): void {
+    console.log('invader saved!');
+    this.editMode = false;
+  }
+  public edit(): void {
+    this.editMode = true;
+  }
 }
