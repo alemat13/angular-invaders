b123a0bd2fb3d8c472398dcb09d14ad54625bbc7
alemat13 <alemat13@gmail.com>
2022-02-06 17:18:56 +0100

    remove FormModules import

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { InvadersModule } from './invaders/invaders.module';
 
 @NgModule({
-  imports: [BrowserModule, FormsModule, AppRoutingModule, InvadersModule],
+  imports: [BrowserModule, AppRoutingModule, InvadersModule],
   declarations: [AppComponent],
   bootstrap: [AppComponent],
 })
