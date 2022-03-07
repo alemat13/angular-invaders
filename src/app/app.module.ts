785453c65f5ca3b4bd8a633731d9357187b139ce
alemat13 <alemat13@gmail.com>
2022-03-07 16:11:38 +0100

    Add HttpClientModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
@@ -9,6 +10,7 @@ import { UsersModule } from './users/users.module';
 @NgModule({
   imports: [
     BrowserModule,
+    HttpClientModule,
     InvadersModule,
     UsersModule,
     AppRoutingModule
