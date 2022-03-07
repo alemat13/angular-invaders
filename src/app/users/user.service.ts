d4a2d50113a097d3b000ca7ac63d499fe376c8dd
alemat13 <alemat13@gmail.com>
2022-03-07 18:09:46 +0100

    Define InMemoryData for Users

--- src/app/users/user.service.ts
+++ src/app/users/user.service.ts
@@ -1,21 +1,42 @@
 import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
-
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { catchError, map, tap } from 'rxjs';
 import { User } from './user';
-import { USERS } from './mock-users';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
+
+  private usersUrl = 'api/users';
+
+  constructor(private http: HttpClient) { }
+
   getUser(id: number): Observable<User> {
-    const user = USERS.find(i => i.id === id)!;
-    return of(user);
+    const url = `${this.usersUrl}/${id}`;
+    return this.http.get<User>(url).pipe(
+      tap(_ => this.log(`fetch user ${id}`)),
+      catchError(this.handleError<User>('getUser'))
+    )
   }
 
-  constructor() { }
   getUsers(): Observable<User[]> {
-    const users = of(USERS);
-    return users;
+    return this.http.get<User[]>(this.usersUrl).pipe(
+      tap(_ => this.log('fetched users')),
+      catchError(this.handleError<User[]>('getUsers, []'))
+    );
+  }
+
+  private log(log: string): void {
+    console.info(log);
+  }
+
+  handleError<T>(operation='operation', result?: T) {
+    return (error: any): Observable<T> => {
+      console.log(error);
+      console.log(`${operation} failed: ${error.message}`);
+      return of(result as T);
+    }
   }
 }
