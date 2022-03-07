f9c3898e2a027c280460572034a2329c35ed101e
alemat13 <alemat13@gmail.com>
2022-03-07 14:40:14 +0100

    Init EditInvaderComponent

---
+++ src/app/invaders/edit-invader/edit-invader.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-edit-invader',
+  templateUrl: './edit-invader.component.html',
+  styleUrls: ['./edit-invader.component.css']
+})
+export class EditInvaderComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/invaders/invaders.module.ts
+++ src/app/invaders/invaders.module.ts
@@ -7,9 +7,10 @@ import { InvadersListComponent } from './invaders-list/invaders-list.component';
 import { InvaderDetailComponent } from './invader-detail/invader-detail.component';
 import { InvaderStatusPipe } from './invader-status.pipe';
 import { InvaderFormComponent } from './invader-form/invader-form.component';
+import { EditInvaderComponent } from './edit-invader/edit-invader.component';
 
 @NgModule({
-  declarations: [InvadersListComponent, InvaderDetailComponent, InvaderStatusPipe, InvaderFormComponent],
+  declarations: [InvadersListComponent, InvaderDetailComponent, InvaderStatusPipe, InvaderFormComponent, EditInvaderComponent],
   imports: [
     CommonModule,
     InvadersRoutingModule,
