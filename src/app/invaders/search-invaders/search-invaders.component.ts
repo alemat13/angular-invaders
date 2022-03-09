35c6ddd2532ca1927a16430dd0d8e0b61d767610
alemat13 <alemat13@gmail.com>
2022-03-09 11:34:49 +0100

    Define SearchInvaders

--- src/app/invaders/search-invaders/search-invaders.component.ts
+++ src/app/invaders/search-invaders/search-invaders.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { debounceTime, distinctUntilChanged, Observable, Subject, switchMap } from 'rxjs';
+import { Invader } from '../invader';
+import { InvaderService } from '../invader.service';
 
 @Component({
   selector: 'app-search-invaders',
@@ -7,9 +11,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SearchInvadersComponent implements OnInit {
 
-  constructor() { }
+  private searchTerms = new Subject<string>();
+  invaders$?: Observable<Invader[]>;
+
+  constructor(
+    invaders$: Observable<Invader[]>,
+    private invaderService: InvaderService,
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
+    this.invaders$ = this.searchTerms.pipe(
+      debounceTime(300),
+      distinctUntilChanged(),
+      switchMap((term: string) => this.invaderService.searchInvaders(term))
+    )
+  }
+
+  search(term: string): void {
+    this.searchTerms.next(term);
+  }
+
+  gotoDetail(invader: Invader): void {
+    let link = ['/invader', invader.id];
+    this.router.navigate(link);
   }
 
 }
