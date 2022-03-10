9216c01228efa221ba448e3c230382a77d65f6f3
alemat13 <alemat13@gmail.com>
2022-03-10 12:15:57 +0100

    Move LoginComponent to LoginModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,7 +9,7 @@ import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { InvadersModule } from './invaders/invaders.module';
 import { UsersModule } from './users/users.module';
-import { LoginComponent } from './login/login.component';
+import { LoginComponent } from './login/login/login.component';
 import { FormsModule } from '@angular/forms';
 import { LoginModule } from './login/login.module';
 
@@ -24,7 +24,7 @@ import { LoginModule } from './login/login.module';
     AppRoutingModule,
     FormsModule, 
   ],
-  declarations: [AppComponent, LoginComponent],
+  declarations: [AppComponent],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
