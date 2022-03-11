4aaf9150696c4ba85a4dadd11da6a2f43d9ade26
alemat13 <alemat13@gmail.com>
2022-03-11 19:56:44 +0100

    Add title

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Invaders Manager';
+  constructor(
+    private header_title: Title
+  ) {}
+  ngOnInit() {
+    this.header_title.setTitle('Invaders Manager - Home page');
+  }
 }
